6b53034eb2ec368b1ab180e6ecfe268619f7c83d
felipesg44 <sierragomezandres@gmail.com>
2025-11-20 14:00:53 -0500

    Ejercicio 2: Obtención de nombres desde lista de usuarios.

--- AndresFelipeSierraGomez/script.js
+++ AndresFelipeSierraGomez/script.js
@@ -5,3 +5,14 @@ let puntosDoble = puntos.map(punto => punto * 2);
 
 console.log(puntos);
 console.log(puntosDoble);
+
+// Ejercicio 2: Lista de Nombres (map)
+
+let usuarios = [
+    { id: 1, nombre: "Ana" },
+    { id: 2, nombre: "Luis" },
+    { id: 3, nombre: "Carlos" }
+];
+
+let nombres = usuarios.map(usuario => usuario.nombre);
+console.log(nombres);
