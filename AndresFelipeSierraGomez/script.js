3850811cf54431bd75e9ab6ecaec76cfc3882a7c
felipesg44 <sierragomezandres@gmail.com>
2025-11-20 15:10:30 -0500

    Ejercicio 16: Inserción sin mutar usando slice y spread.

--- AndresFelipeSierraGomez/script.js
+++ AndresFelipeSierraGomez/script.js
@@ -120,3 +120,14 @@ let fila = ["Ana", "Luis", "Carlos", "Maria"];
 let nuevaFila = fila.filter(nombre => nombre !== "Luis");
 
 console.log(nuevaFila);
+
+// Ejercicio 16: Insertar sin Mutar (slice + spread)
+
+let fila2 = ["Ana", "Luis", "Carlos", "Maria"];
+let nuevaFilaConJorge = [
+    ...fila2.slice(0, 1),
+    "Jorge",
+    ...fila2.slice(1)
+];
+
+console.log(nuevaFilaConJorge);
